2bee8bddbdff7fe522314168af426f0f6385a360
Ruslan Lekso <ruslanlesko@gmail.com>
2020-05-12 16:31:32 +0300

    Update version

--- src/auth.go
+++ src/auth.go
@@ -218,6 +218,6 @@ func main() {
 	dao := NewMongoUserDao(conf.MongoDBURL, conf.DatabaseName)
 	service := AuthService{UserDao: dao, Config: conf}
 	auth := Auth{AuthService: &service}
-	logger.Logf("INFO BrightonUM 1.2.0 is starting")
+	logger.Logf("INFO BrightonUM 1.2.1 is starting")
 	auth.start()
 }
