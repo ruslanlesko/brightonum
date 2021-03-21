fa9e3095ebf594cfaf9ec73d60bcc02e263cff5f
Ruslan Lesko <ruslanlesko@gmail.com>
2021-03-21 10:53:33 +0200

    Bump up version

--- src/auth.go
+++ src/auth.go
@@ -382,6 +382,6 @@ func main() {
 	mailer := EmailMailer{Email: conf.Email, Password: conf.EmailPassword}
 	service := AuthService{UserDao: dao, Mailer: &mailer, Config: conf}
 	auth := Auth{AuthService: &service}
-	logger.Logf("INFO BrightonUM 1.5.4 is starting")
+	logger.Logf("INFO BrightonUM 1.6.0 is starting")
 	auth.start()
 }
